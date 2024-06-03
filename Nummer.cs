477c8bcbde3ccefaee695a20f3744394949a40a4
odinVos <1197607@student.roc-nijmegen.nl>
2024-06-03 15:34:28 +0200

    added genre

---
+++ Nummer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Nummer
+{
+    int id;
+    string name = "Nummer 1";
+    string artiest = "test";
+    double lengte = 4.35;
+    enum genre
+    {
+        blues,
+        jazz,
+        country,
+        metal
+    }
+    public void getGenre()
+    {
+        genre nummer1 = genre.blues;
+        Console.WriteLine(nummer1);
+    }
+
+    public void getNaam()
+    {
+        Console.WriteLine(name);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            Nummer Nummer = new Nummer();
+            Nummer.getNaam();
+            Nummer.getGenre();
         }
+
     }
+
 }
