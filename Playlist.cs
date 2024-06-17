ac1c4a495fed4876c0baaa4d44a3179e715f21b2
odinVos <1197607@student.roc-nijmegen.nl>
2024-06-17 14:29:52 +0200

    test kist

---
+++ Playlist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public List<Number> Numbers { get; set; }
+
+        public void RemoveNumber(Number number)
+        {
+            Numbers.Remove(number);
+        }
+}
