699e7cd948c0ed19d06ada722c074751579465b8
ineedausernamehelp <143808946+JvdKolkIctoria@users.noreply.github.com>
2024-06-28 19:19:11 +0200

    added friend class

---
+++ Friend.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotivy
+{
+    class Friends
+    {
+        List<User> friends { get; set; }
+
+        public Friends() {
+        }
+
+        public List<User> addFriend(User user) {
+        
+             friends.Add(user);
+            Console.WriteLine("added friend:" + user.UserName);
+            return friends;
+        }
+        public List<User> deleteFriend(User user)
+        {
+            friends.Remove(user);
+            Console.WriteLine("removed friend:" + user.UserName);
+            return friends;
+        }
+
+        public void showFriendList()
+        {
+            foreach (User friend in friends)
+            {
+                Console.WriteLine(friend.UserName);
+            }
+        }
+
+
+    }
+}
