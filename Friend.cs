43895b91ac72ae3521e3c0db2a2b66ec07c6736b
odinVos <1197607@student.roc-nijmegen.nl>
2024-06-28 20:51:08 +0200

    Friend gefixt

---
+++ Friend.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotivy
+{
+    class Friends
+    {
+        List<User> friends { get; set; }
+        public Friends()
+        {
+            friends = new List<User>();
+        }
+
+        public List<User> addFriend(User user)
+        {
+
+            if (user == null)
+            {
+                return friends;
+            }
+            friends.Add(user);
+            Console.WriteLine("added friend:" + user.UserName);
+            return friends;
+        }
+        public List<User> deleteFriend(User user)
+        {
+            friends.Remove(user);
+            Console.WriteLine("removed friend:" + user.UserName);
+            return friends;
+        }
+
+        public void showFriendList()
+        {
+            foreach (User friend in friends)
+            {
+                Console.WriteLine(friend.UserName);
+            }
+        }
+
+
+
+    }
+}
--- Songlist.cs
+++ Songlist.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Spotivy.Users
+namespace Spotivy
 {
     internal class Songlist
     {
